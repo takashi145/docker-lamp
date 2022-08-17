5095fb62c47a1cc6d0e15117e4ceab117d818614
takashi145 <t.shuhei0203@gmail.com>
2022-08-17 18:41:57 +0900

    first commit

---
+++ docker/db/sql/init.sql
@@ -0,0 +1,7 @@
+-- CREATE TABLE IF NOT EXISTS test_table (
+--   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+--   title VARCHAR(30) NOT NULL,
+--   body TEXT
+-- ) charset=utf8;
+
+-- INSERT INTO test_table VALUES(NULL, "test1", "testtest"),(NULL, "test2", "test2test2");
